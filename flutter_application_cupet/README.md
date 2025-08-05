28cefc1b26b73dbca535120748dd1f2ea8e2a226
hmza saleh aljpour <137841148+Hm1za@users.noreply.github.com>
2025-08-05 12:13:23 +0300

    Delete flutter_application_cupet directory

--- flutter_application_cupet/README.md
+++
@@ -1,3 +0,0 @@
-# flutter_application_cupet
-
-A new Flutter project.
--- flutter_application_cupet/lib/src/counter/cubit/counter_state.dart
+++
@@ -1,5 +0,0 @@
-part of 'counter_cubit.dart';
-
-sealed class CounterState {}
-
-final class CounterInitial extends CounterState {}
